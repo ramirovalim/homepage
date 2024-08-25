03a7b5cdd60990caef51f1c34a83e2abf88d8e6d
Ramiro Valim <ramirotuk@gmail.com>
2024-08-25 00:50:42 -0300

    changin file structure

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files from the '' directory
-app.use(express.static(""));
+app.use(express.static("./public", { index: "index.html" }));
 
 // Handle 404 - Keep this as the last route
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "", "404.html"));
+});
+
+app.get("../*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
